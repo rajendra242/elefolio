a7bd747e9cf4202547cf0f34dc01aac47d0d3f72
Rajendra Sinh Rathod <rajendrasinhrathod779@gmail.com>
2023-05-30 10:26:43 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
+import { SkilsComponent } from './skils/skils.component';
+import { ClientsComponent } from './clients/clients.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectComponent,
+    SkilsComponent,
+    ClientsComponent,
+    SubscriptionComponent
   ],
   imports: [
     BrowserModule,
